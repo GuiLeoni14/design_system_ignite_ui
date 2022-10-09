109399d2b93927b0546d12975187663615a89244
GuiLeoni14 <guileone167@gmail.com>
2022-10-09 19:54:42 -0300

    configurando storybook

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,19 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta<ButtonProps> // obj global todas as variações herdam dele {children: "Enviar"}
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Enviar',
+  },
+}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
