7d450e9a441f2bb4a980d7a02fe7b9d5715179f6
GuiLeoni14 <guileone167@gmail.com>
2022-10-28 13:38:13 -0300

    ajustando tokens grid

--- packages/docs/src/components/TokensGrid.tsx
+++ packages/docs/src/components/TokensGrid.tsx
@@ -1,14 +1,17 @@
+import '../styles/tokens-grid.css'
 interface TokensGridProps {
   tokens: Record<string, string>
+  hasRemValue?: boolean
 }
 
-export function TokensGrid({ tokens }: TokensGridProps) {
+export function TokensGrid({ tokens, hasRemValue = false }: TokensGridProps) {
   return (
-    <table>
+    <table className="tokens-grid">
       <thead>
         <tr>
           <th>Name</th>
           <th>Value</th>
+          {hasRemValue && <th>Pixels</th>}
         </tr>
       </thead>
       <tbody>
@@ -17,6 +20,9 @@ export function TokensGrid({ tokens }: TokensGridProps) {
             <tr key={value}>
               <td>{key}</td>
               <td>{value}</td>
+              {hasRemValue && (
+                <td>{Number(value.replace('rem', '')) * 16}px</td>
+              )}
             </tr>
           )
         })}
